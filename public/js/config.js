ffba8d4a936f375388d82a0e405ec6af4428d40b
financesmartacc101-1994 <finance.smartacc101@gmail.com>
2026-09-25 11:41:46 +0700

    Add files via upload

---
+++ public/js/config.js
@@ -0,0 +1,4 @@
+// ใส่ URL ของ Apps Script Web App ที่นี่ หลังจาก Deploy > New deployment > Web app
+// ตัวอย่าง: https://script.google.com/macros/s/xxxxxxxxxxxxxxxx/exec
+window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxkCUf_GxmRKisZk3kyB5utgW2DueLXrkY-cO1XcJudPHHC84tHnSwKRU0zBHGNF2HwbQ/exec';
+window.SHOP_NAME = 'ร้าน นั่งปั่นจักยาน';
